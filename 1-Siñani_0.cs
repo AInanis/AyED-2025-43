70a22a6915305ad30a827ccf9d27b4f9345498de
AInanis <abisinani24@gmail.com>
2025-05-14 23:23:43 -0300

    Etapa1

---
+++ 1-Siñani_0.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            string contraseñaU;
+            string contraseñaGuardada = "PROGRAMACION";
+            Console.Write("Introduce la contraseña: ");
+            contraseñaU = (Console.ReadLine().Toupper);
+            if (contraseñaU == contraseñaGuardada)
+            {
+                Console.WriteLine("Exacto!,la contraseña es" + contraseñaU);
+            }
+            else
+            {
+                Console.WriteLine("Contraseña incorrecta..");
+            }
+            Console.ReadKey();
+
+
+        }
+    }
+}
