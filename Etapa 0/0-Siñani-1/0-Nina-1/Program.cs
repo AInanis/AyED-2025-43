96c004bd8feef5bf3b096cd08ac31451672a010c
AInanis <abisinani24@gmail.com>
2025-05-20 11:54:41 -0300

    Add files via upload

---
+++ Etapa 0/0-Siñani-1/0-Nina-1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0_Nina_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string hola;
+            hola = Console.ReadLine();
+
+            Console.WriteLine(hola);
+            Console.ReadKey();
+        }
+    }
+}
