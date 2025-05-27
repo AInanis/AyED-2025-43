9a374269e318a22c367ae5ad98bbb24b7ef48124
AInanis <abisinani24@gmail.com>
2025-05-26 21:13:33 -0300

    Add files via upload

---
+++ Etapa 1/1-Siñani_9/1-Nina_9/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_Nina_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Num;
+            Console.WriteLine("Dime un numero");
+            Num = Convert.ToInt32(Console.ReadLine());
+
+            for (int I = 0; Num > -1; I++ )
+            {
+                Console.WriteLine("Tu numero es " + Num);
+                Num = Num - 1;
+            }
+            Console.ReadKey();
+        }
+    }
+}
