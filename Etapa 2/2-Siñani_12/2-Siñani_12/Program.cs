572cabd2bdeda80164407339041d3fa83c074f12
AInanis <abisinani24@gmail.com>
2025-10-01 21:02:20 -0300

    Add files via upload

---
+++ Etapa 2/2-Siñani_12/2-Siñani_12/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_Siñani_12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int n;
+
+            Console.Write("Ingrese el tamaño de la matriz (n): ");
+            n = int.Parse(Console.ReadLine());
+
+            int[,] matriz = new int[n, n];
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    matriz[i, j] = rnd.Next(1, 100);
+                }
+            }
+
+            Console.WriteLine("\nMatriz generada:");
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write(matriz[i, j].ToString("D2") + " ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("\nDiagonal principal (como matriz):");
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (i == j)
+                        Console.Write(matriz[i, j].ToString("D2") + " ");
+                    else
+                        Console.Write("   "); 
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
